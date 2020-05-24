79b43cfbb2427047aeba5cbe18904659d5006664
BERRAK PERK <bperk15@ku.edu.tr>
2020-05-24 19:23:49 +0300

    Class creation

---
+++ src/Node.java
@@ -0,0 +1,41 @@
+import java.util.Hashtable;
+
+public class Node {
+    static int NodeID;
+    public Hashtable<Integer,Integer> linkCost= new Hashtable<Integer,Integer>();
+    public Hashtable<Integer,Integer> linkBandwidth= new Hashtable<Integer,Integer>();
+    static int distanceTable[][];
+    static int bottleneckBandwidthTable[][];
+
+
+    public Node(int nodeID, HashTable<Integer,Integer>  linkCost, HashTable<Integer,Integer>  linkBandwidth){
+
+
+    }
+
+    public void receiveUpdate(Message m){
+
+
+    }
+
+    public boolean sendUpdate(){
+
+
+    }
+
+
+    public HashTable<> getForwardingTable(){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
